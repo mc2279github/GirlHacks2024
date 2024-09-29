3853b0c7fed7155eb8fae10edf07b212ae73733b
mc2279github <mc2279@njit.edu>
2024-09-29 01:39:20 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use hyper_util::rt::tokio::{TokioExecutor, TokioIo};
 use hyper_util::server::conn::auto::Builder;
 use rustls_pemfile::{certs, private_key};
 use rustls_pki_types::CertificateDer;
-use std::env;
 use std::fs;
 use std::io::{self, BufReader};
 use std::sync::Arc;
@@ -16,9 +15,9 @@ use tokio_rustls::TlsAcceptor;
 
 async fn service(req: Request<Incoming>) -> io::Result<Response<Full<Bytes>>> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "") => Ok(Response::builder()
+        (&Method::GET, "/assets/logo.jpeg") => Ok(Response::builder()
             .status(200)
-            .body(Full::from(tokio::fs::read("").await?))
+            .body(Full::from(tokio::fs::read("assets/logo.jpeg").await?))
             .unwrap()),
         (&Method::POST, "") => Ok(Response::builder()
             .status(200)
@@ -26,28 +25,26 @@ async fn service(req: Request<Incoming>) -> io::Result<Response<Full<Bytes>>> {
             .unwrap()),
         _ => Ok(Response::builder()
             .status(404)
-            .body(Full::from(tokio::fs::read("").await?))
+            .body(Full::from(tokio::fs::read("assets/404.html").await?))
             .unwrap()),
     }
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let addr = args.get(1).unwrap();
-    let key = private_key(&mut BufReader::new(fs::File::open(&args[2])?)).unwrap();
+    let key = private_key(&mut BufReader::new(fs::File::open("key.pem")?)).unwrap();
     let cert: io::Result<Vec<CertificateDer>> =
-        certs(&mut BufReader::new(fs::File::open(&args[3]).unwrap())).collect();
+        certs(&mut BufReader::new(fs::File::open("cert.pem").unwrap())).collect();
     let mut config = ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(cert?, key.unwrap())
         .unwrap();
     config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
     let acceptor = TlsAcceptor::from(Arc::new(config));
-    let listner = TcpListener::bind(addr).await.unwrap();
+    let listner = TcpListener::bind("0.0.0.0:4443").await?;
     let service = service_fn(service);
     loop {
-        let (stream, addr) = listner.accept().await?;
+        let (stream, _) = listner.accept().await.unwrap();
         let acceptor = acceptor.clone();
         tokio::spawn(async move {
             let stream = acceptor.accept(stream).await.unwrap();
